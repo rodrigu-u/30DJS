0ac003d7a9280a10caee61e684ecd352b211f92b
Rodrigo <rodrigoesmeraldo.1999@gmail.com>
2024-05-02 19:21:29 -0300

    first commit

---
+++ Day 1/script.js
@@ -0,0 +1,46 @@
+/* 1 */ //comments can make code readable
+
+/* 2 */ //Welcome to 30DaysOfJavaScript
+
+/* 3 */ /* comments can make code readable,
+easy to reuse and informative */
+
+/* 4 */
+let string = "Rodrigo";
+let boolean = true;
+let noValue = undefined;
+let nothing = null;
+
+/* 5 */
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof noValue);
+console.log(typeof nothing);
+
+/* 6 */
+let u1;
+let u2;
+let u3;
+let u4;
+
+/* 7 */
+let n1 = 1;
+let n2 = 2;
+let n3 = 3;
+let n4 = 4;
+
+/* 8 */
+let firstName1;
+let lastName1;
+let maritalStatus1;
+let country1;
+let age1;
+
+/* 9 */
+let firstName, lastName, maritalStatus, country, age;
+
+/* 10 */
+let myAge = 24;
+let yourAge = 25;
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${yourAge} years old.`);
